7c1e642d3304bc3c3be593ed56c3ff2169d61aed
阿部直也 <n.abe@gemcook.com>
2020-05-25 11:07:25 +0900

    Added ProfileFeedAPI.

--- profiles-rest-api/profiles_api/serializers.py
+++ profiles-rest-api/profiles_api/serializers.py
@@ -8,9 +8,10 @@ from profiles_api import models
     ・Model(レコード)を適切な形式にフォーマットする(出力：デシリアライズ)
 """
 
+
 class HelloSerializers(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
-    name=serializers.CharField(max_length=10)
+    name = serializers.CharField(max_length=10)
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
@@ -22,7 +23,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
         # 管理したい項目を定義（タプル形式）
         fields = ('id', 'email', 'name', 'password')
         extra_kwargs = {
-            'password':{
+            'password': {
                 # セキュリティの関係上パスワードは書き込むだけ。
                 'write_only': True,
                 # パスワード入力の際に「・・・」となるようにstyleを指定
@@ -32,9 +33,9 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
     # ModelSerializerにデフォルトでcreate(),update()が実装されているが
     # passwordがハッシュ化されないので、それぞれオーバーライドする
-    def create(self,validated_data):
+    def create(self, validated_data):
         """Create and return a new user"""
-        user=models.UserProfile.objects.create_user(
+        user = models.UserProfile.objects.create_user(
             email=validated_data['email'],
             name=validated_data['name'],
             password=validated_data['password']
@@ -42,10 +43,18 @@ class UserProfileSerializer(serializers.ModelSerializer):
 
         return user
 
-    def update(self,instance,validated_data):
+    def update(self, instance, validated_data):
         """Handle updating user account"""
         if 'password' in validated_data:
-            password=validated_data.pop('password')
+            password = validated_data.pop('password')
             instance.set_password(password)
 
-        return super().update(instance,validated_data)
+        return super().update(instance, validated_data)
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+    class Meta():
+        model = models.ProfilesFeedItem
+        fields = ('id', 'user_profile', 'created_on', 'status_text')
+        extra_kwargs = {'user_profile': {'read_only': True}}
